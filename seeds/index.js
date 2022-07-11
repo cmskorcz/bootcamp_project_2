11b4791a0b1e8c2ab5cd059e5368a4e486655e2a
cmskorcz <chaseskorey@icloud.com>
2022-07-11 19:40:48 -0400

    comment seeds complete

--- seeds/index.js
+++ seeds/index.js
@@ -2,6 +2,7 @@ const sequelize = require('../config/connection');
 
 const seedUser = require('./user-seeds');
 const seedEvents = require('./event-seeds');
+const seedComments = require('./comment-seeds');
 
 const seedAll = async () => {
     await sequelize.sync({ force: true });
@@ -9,7 +10,9 @@ const seedAll = async () => {
     await seedUser();
     console.log('Users seeded');
     await seedEvents();
-    console.log('Events seeded')
+    console.log('Events seeded');
+    await seedComments();
+    console.log('Comments Seeded');
 };
 
 module.exports = seedAll;

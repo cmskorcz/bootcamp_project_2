49b55cae2e0a0db999b6b282d4d974c6d2962fb0
Bre <breanna.schmidt12@gmail.com>
2022-07-12 17:50:45 -0400

    fixed typo

--- server.js
+++ server.js
@@ -40,5 +40,5 @@ app.use(routes);
 
 
 sequelize.sync({ force: false }).then(() => {
- app.listen(PORT, () => console.log(`Now listening on ${PORT}`));
+ app.listen(PORT, () => console.log(`Now listening on http://localhost:${PORT}`));
 });

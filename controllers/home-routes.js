1bf09056828d652a3861f1cd5a5061b2242717ae
MIshmail <michaely.ishmail@gmail.com>
2022-07-13 15:49:19 -0400

    adjustments

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,8 @@
 const router = require('express').Router()
 const sequelize = require('../config/connection');
 const { Event } = require('../models');
-
+const users = []
+const bcrypt = require('bcrypt');
 
 router.get('/', (req, res) => {
     return res.render('home')
@@ -11,9 +12,33 @@ router.get('/login', (req, res) => {
     return res.render('login')
 })
 
+/* router.post('/login', (req, res) => {
+
+})
+*/
+
 router.get('/signup', (req, res) => {
     return res.render('signup')
 })
+
+/* router.post('/signup', async (req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        users.push ({
+            id: Date.now().toString(),
+            email: req.body.email,
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            password: hashedPassword
+        })
+        res.redirect('/login')
+    } catch {
+        res.redirect('/signup')
+
+    }
+    console.log(users)
+})
+*/
 
 router.get('/dashboard', (req, res) => {
     return res.render('events')
--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const seedAll = require('./seeds/index');
+const bcrypt = require('bcrypt')
+const passport = require('passport')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -28,7 +30,7 @@ app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static(path.join(__dirname, 'public')));
 

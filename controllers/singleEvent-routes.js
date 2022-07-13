df027790a5bb7ed2b8423d9c154e7688199aa010
Bre <breanna.schmidt12@gmail.com>
2022-07-13 18:50:04 -0400

    changed comment to req.body

--- controllers/singleEvent-routes.js
+++ controllers/singleEvent-routes.js
@@ -70,9 +70,7 @@ router.get("/:id", async (req, res) => {
 router.put("/:id", async (req, res) => {
   try {
     const updateEvent = await Event.update(
-      {
-        //  ??
-      },
+       req.body,
       {
         where: {
           id: req.params.id,

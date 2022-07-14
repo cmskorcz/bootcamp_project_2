a0b4c3d5496c8fda7f5acc076657890859dffbc0
cmskorcz <chaseskorey@icloud.com>
2022-07-13 22:32:24 -0400

    events now dynamically post to homepage

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,10 +1,34 @@
 const router = require('express').Router()
 const sequelize = require('../config/connection');
-const { Event } = require('../models');
+const { Event, User } = require('../models');
 
 
-router.get('/', (req, res) => {
-    return res.render('home')
+router.get('/', async (req, res) => {
+    try {
+        const eventsArr = await Event.findAll({
+            attributes: [
+                'id',
+                'name',
+                'description',
+                'address',
+                'date'
+            ],
+            include: [
+                {
+                    model: User,
+                    attributes: [
+                        'id',
+                        'first_name'
+                    ]
+                }
+            ]
+        });
+        const events = eventsArr.map(event => event.get({ plain: true }));
+    
+        return res.render('home', { events });    
+    } catch (error) {
+        res.status(500).json(error)
+    }
 })
 
 router.get('/login', (req, res) => {

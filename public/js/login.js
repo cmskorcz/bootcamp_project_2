bd5911bfc3b2383b35103fef0648618ae19bf87c
MIshmail <michaely.ishmail@gmail.com>
2022-07-14 20:40:06 -0400

    researching bugs during login/viewing the profile

--- public/js/login.js
+++ public/js/login.js
@@ -5,7 +5,7 @@ async function loginFormHandler(event) {
     const password = document.querySelector('#login-password').value.trim()
 
     if (email && password) {
-        const response = await fetch('/login', {
+        const response = await fetch('/api/login', {
             method: 'post',
             body: JSON.stringify({
                 email,
